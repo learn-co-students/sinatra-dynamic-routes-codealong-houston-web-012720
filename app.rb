c8891ee90e1a3b30b75b8edf7aafa9e05d7b0f1c
DrVonDevious <drvondevious@gmail.com>
2020-02-14 23:38:40 -0600

    Done.

--- app.rb
+++ app.rb
@@ -4,15 +4,28 @@ class App < Sinatra::Base
 
   # This is a sample static route.
   get '/hello' do
+    erb :index
     "Hello World!"
   end
 
   # This is a sample dynamic route.
   get "/hello/:name" do
+    erb :hello
     @user_name = params[:name]
     "Hello #{@user_name}!"
   end
 
   # Code your final two routes here:
+
+  get '/goodbye/:name' do
+    erb :goodbye
+    @user_name = params[:name]
+    "Goodbye, #{@user_name}."
+  end  
+
+  get '/multiply/:num1/:num2' do
+    @sum = params[:num1].to_i * params[:num2].to_i
+    "#{@sum}"
+  end  
 
 end
